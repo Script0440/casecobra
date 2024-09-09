6cff2b2792b9e93205b0b6b86ab4d206cf67254b
Arman Minasyan <juzojuzo23@gmail.com>
2024-09-09 04:03:15 +0400

    update stuff

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Confetti from "react-dom-confetti";
 import { Configuration } from "@prisma/client";
 import Phone from "@/components/Phone";
-import { COLORS, MODELS, FINISHES } from "@/validators/option-validator";
+import { COLORS, MODELS } from "@/validators/option-validator";
 import { cn, formatPrice } from "@/lib/utils";
 import { ArrowRight, Check } from "lucide-react";
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
@@ -58,7 +58,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     },
   });
 
-  const habdleCheckout = () => {
+  const handleCheckout = () => {
     if (user) {
       //create payment session
       createPaymentSession({ configId: id });
@@ -90,7 +90,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
             imgSrc={configuration.croppedImageUrl!}
           />
         </div>
-        <div className="mt-6 sm:col-span-9 sm:mt-0 md:row-end-1">
+        <div className="mt-6 sm:col-span-9 md:row-end-1">
           <h3 className="text-3xl font-bold tracking-tight text-gray-900">
             Your {modelLabel} Case
           </h3>
@@ -161,7 +161,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
             <div className="mt-8 flex justify-end pb-12">
               <Button
-                onClick={() => habdleCheckout()}
+                onClick={() => handleCheckout()}
                 className="px-4 sm:px-6 lg:px-8 "
               >
                 Check out <ArrowRight className="h4- w-4 ml-1.5 inline" />
