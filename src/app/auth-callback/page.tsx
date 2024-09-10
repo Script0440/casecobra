8dd1a2f9c8836bb58681812086d34ce9cfc62c12
Arman Minasyan <juzojuzo23@gmail.com>
2024-09-10 22:48:36 +0400

    New commit message

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,9 +2,9 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { getAuthStatus } from "./actions";
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
-import { getAuthStatus } from "./actions"; // Импортируй функцию из файла
 
 const Page = () => {
   const [configId, setConfigId] = useState<string | null>(null);
@@ -15,48 +15,31 @@ const Page = () => {
     if (configurationId) setConfigId(configurationId);
   }, []);
 
-  const { data, isLoading, isError } = useQuery({
+  const { data } = useQuery({
     queryKey: ["auth-callback"],
-    queryFn: getAuthStatus, // Здесь вызываем серверную функцию напрямую
+    queryFn: async () => await getAuthStatus(),
     retry: true,
     retryDelay: 500,
   });
 
-  useEffect(() => {
-    if (data?.success) {
-      if (configId) {
-        localStorage.removeItem("configurationId");
-        router.push(`/configure/preview?id=${configId}`);
-      } else {
-        router.push("/");
-      }
+  if (data?.success) {
+    if (configId) {
+      localStorage.removeItem("configurationId");
+      router.push(`/configure/preview?id=${configId}`);
+    } else {
+      router.push("/");
     }
-  }, [data, configId, router]);
-
-  if (isLoading) {
-    return (
-      <div className="w-full mt-24 flex justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
-          <h3 className="font-semibold text-xl">Logging you in...</h3>
-          <p>You will be redirected automatically.</p>
-        </div>
-      </div>
-    );
   }
 
-  if (isError) {
-    return (
-      <div className="w-full mt-24 flex justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <h3 className="font-semibold text-xl">Error logging in</h3>
-          <p>There was a problem with the login process. Please try again.</p>
-        </div>
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h8 w-8 animate-spin text-zinc-500" />
+        <h3 className="font-semibold text-xl">Logging you in...</h3>
+        <p>You will be redirected automatically.</p>
       </div>
-    );
-  }
-
-  return null;
+    </div>
+  );
 };
 
 export default Page;
